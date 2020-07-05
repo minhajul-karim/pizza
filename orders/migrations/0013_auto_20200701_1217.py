3e0a4129c158e4dae1622b2d2ad715b2eeb0f43b
minhajul-karim <minhajul.kaarim@gmail.com>
2020-07-05 16:14:21 +0600

    Generate datadump.json from sqlite

--- orders/migrations/0013_auto_20200701_1217.py
+++ orders/migrations/0013_auto_20200701_1217.py
@@ -14,13 +14,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Status',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True, serialize=False, verbose_name='ID')),
                 ('status_name', models.CharField(max_length=50)),
             ],
         ),
         migrations.AlterField(
             model_name='order',
             name='status',
-            field=models.ForeignKey(default='not confirmed', on_delete=django.db.models.deletion.CASCADE, to='orders.Status'),
+            field=models.ForeignKey(
+                default=1, on_delete=django.db.models.deletion.CASCADE, to='orders.Status'),
         ),
     ]
